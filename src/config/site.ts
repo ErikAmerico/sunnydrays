a6409d14ec47e0b77b1b02d6c993302eb4b6b162
Erik Olson <erikamerico1991@gmail.com>
2025-12-23 14:01:30 -0500

    add site informaton file

---
+++ src/config/site.ts
@@ -0,0 +1,14 @@
+export const SITE = {
+  name: "Sunny Dray's Cleaning",
+  domain: "example.com", // set to your real domain later
+  defaultTitle: "Sunny Dray's Cleaning",
+  defaultDescription:
+    "Sunny Dray's Cleaning provides residential cleaning across Southern New Hampshire and the Merrimack Valley.",
+  phone: "+16034535916",
+  phoneDisplay: "(603) 453-5916",
+  email: "sunnydrayscleaning@gmail.com",
+  cityLine: "Plaistow, NH",
+  jotformId: "253528374720055",
+  facebookUrl: "https://www.facebook.com/profile.php?id=61584865443246",
+  instagramUrl: "https://www.instagram.com/sunnydrayscleaning/",
+};
