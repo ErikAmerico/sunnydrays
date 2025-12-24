fc890317326e56461006a418e59eff86480d2f0c
Erik Olson <erikamerico1991@gmail.com>
2025-12-24 01:02:34 -0500

    install astro site map

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
-
+import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  site: "https://sunnydrayscleaning.com",
+  integrations: [sitemap()],
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
